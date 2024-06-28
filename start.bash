6b4e3b994ae95f4db7563d39a5fcb64426056997
Dephiou <168378928+Dephiou@users.noreply.github.com>
2024-06-28 22:39:26 +0200

    Update and rename start.bash to start.bat

--- start.bash
+++
@@ -1,5 +0,0 @@
-# Modifier l'association des fichiers .bash vers CMD
-ftype bashfile="%SystemRoot%\system32\cmd.exe" /c "%1"
-
-
-python3 main.py
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Exécution du script main.py...
+python main.py
+pause
